2087e34c09fa00b8c36bb53b1cbc31a7dacd62fb
costi <costi@DESKTOP-886NS64>
2023-11-18 17:37:18 -0600

    Agregar archivos de proyecto.

---
+++ COM_DCOM/SERVIDOR.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM_DCOM
+{
+    internal class SERVIDOR
+    {
+        [ComVisible(true)]
+        public class Servidor
+        {
+            [ComVisible(true)]
+            public string ObtenerSaludo(string n)
+            {
+                return $"hi, {n}!";
+            }
+        }
+    }
+}
